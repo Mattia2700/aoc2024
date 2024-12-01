ebc3d88c1d3128df430e697d62f5c4078440ee22
Mattia Franzin <mattia2700franzin+dev@gmail.com>
2024-12-01 13:45:36 +0100

    feat: day1

---
+++ day1/part2.py
@@ -0,0 +1,15 @@
+import os
+
+data = open(os.path.dirname(__file__) + '/input.txt', 'r').read().split('\n')
+
+left = []
+right = []
+
+for line in data:
+    values = [int(x) for x in line.split(' ') if x]
+    left.append(values[0])
+    right.append(values[1])
+
+scores = [left[i] * right.count(left[i]) for i in range(len(left))]
+
+print(sum(scores))
